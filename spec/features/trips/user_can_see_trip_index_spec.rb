8e9f1cf161affc7cd0796cb304c50b795beee560
Megan Arellano <ssciolist@gmail.com>
2018-04-09 19:35:14 -0600

    adds trips index test, trips resources to routes, and trip controller

---
+++ spec/features/trips/user_can_see_trip_index_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'Trip Index' do
+  describe 'user visits trip index page' do
+    it 'shows the first 30 trips and all their attributes' do
+      trip1, trip2, trip3, trip4, trip5, trip6, trip7, trip8, trip9, trip10,
+      trip11, trip12, trip13, trip14, trip15, trip16, trip17, trip18, trip19,
+      trip20, trip21, trip22, trip23, trip24, trip25, trip26, trip27, trip28,
+      trip29, trip30, trip31 = create_list(:trip, 31)
+
+      visit trips_path
+
+      expect(page).to have_content(trip1.duration)
+      expect(page).to have_content(trip1.start_date)
+      expect(page).to have_content(trip1.start_station_id)
+      expect(page).to have_content(trip1.start_station_name)
+      expect(page).to have_content(trip1.end_date)
+      expect(page).to have_content(trip1.end_station_id)
+      expect(page).to have_content(trip1.end_station_name)
+      expect(page).to have_content(trip1.bike_id)
+      expect(page).to have_content(trip1.subscription_type)
+      expect(page).to have_content(trip1.zip_code)
+      expect(page).to have_content(trip30.duration)
+      expect(page).to_not have_content(trip31.duration)
+    end
+
+    it 'shows a button to see more pages of trips' do
+    end
+  end
+
+  describe 'user visits second trip index page' do
+    it 'shows 30 more trips and buttons to move forward and backward' do
+    end
+  end
+end
