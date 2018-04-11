16e8df5ec19e6d90fa6fbbb998d57c5a41136324
Megan Arellano <ssciolist@gmail.com>
2018-04-11 13:56:53 -0600

    minor cart refactor, failing cart test for registered user

---
+++ spec/features/cart/registered_user_can_checkout_their_cart_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'As a registered user' do
+  describe 'when I visit /cart' do
+    it 'I see accessory attributes, subtotal and quantity, and cart total' do
+    end
+
+    it 'I see a button to checkout' do
+    end
+  end
+end
