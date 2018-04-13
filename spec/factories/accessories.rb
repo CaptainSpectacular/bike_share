59854bf163156c4b8a2d6b5182d2b811aac1bf4c
alexcutschall <alexander.cutschall@gmail.com>
2018-04-12 19:04:59 -0600

    adding in test for when you logout

--- spec/factories/accessories.rb
+++ spec/factories/accessories.rb
@@ -4,11 +4,13 @@ FactoryBot.define do
     title "Gear"
     price "9.99"
     image "http://my_string.com/something.png"
+    status " active"
   end
   factory :accessory_2, class: Accessory do
     id 2
     title "Chain"
     price "9.99"
     image "http://my_string.com/something.png"
+    status "inactive"
   end
 end
--- spec/features/accessories/user_can_visit_an_accessory_show_page_spec.rb
+++ spec/features/accessories/user_can_visit_an_accessory_show_page_spec.rb
@@ -4,14 +4,25 @@ describe 'user visits an accessory show page' do
   scenario "it sees all the accessory's information" do
     accessory = create(:accessory)
 
+    visit "/accessory/#{accessory.id}"
+
+    expect(page).to have_content(accessory.title)
+    expect(page).to have_content(accessory.price)
+    expect(page).to have_css("img[src*='http://my_string.com/something.png']")
+
+    click_on "Add to Cart"
+    expect(current_path).to eq('/cart')
+  end
+  scenario "it sees a retired accessories information" do
+    accessory = create(:accessory_2)
+
     visit accessory_path(accessory)
 
     expect(page).to have_content(accessory.title)
     expect(page).to have_content(accessory.price)
     expect(page).to have_css("img[src*='http://my_string.com/something.png']")
 
-
-    click_on "Add to Cart"
-    expect(current_path).to eq('/cart')
+    expect(page).to have_content("Sorry, Accessory is Retired!")
+    expect(page).to_not have_content("Add to Cart")
   end
 end
