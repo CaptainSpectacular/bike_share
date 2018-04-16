fc028956e955a896edc61a99379d3d9379c70454
Megan Arellano <ssciolist@gmail.com>
2018-04-16 11:27:33 -0600

    updates order model to validate purchase name and address, updates factories too

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -3,25 +3,35 @@ FactoryBot.define do
     status "MyString"
     date_time Time.now
     user_id 1
+    purchaser_name "Megan"
+    address "my house"
   end
   factory :paid_order, class: Order do
     status "Paid"
     date_time Time.now
     user_id 1
+    purchaser_name "Jacob"
+    address "Jacob's house"
   end
   factory :orderered_order, class: Order do
     status "Ordered"
     date_time Time.now + 40000
     user_id 1
+    purchaser_name "Mike"
+    address "Mike's house"
   end
   factory :cancelled_order, class: Order do
     status "Cancelled"
     date_time Time.now
     user_id 1
+    purchaser_name "Laura"
+    address "Laura's house"
   end
   factory :completed_order, class: Order do
     status "Completed"
     date_time Time.now
     user_id 1
+    purchaser_name "Jennifer"
+    address "Jennifer's house"
   end
 end
