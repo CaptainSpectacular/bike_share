158f27b7c386d72ad9cd92c83472633fdf48035a
Alex Cutschall <alexander.cutschall@gmail.com>
2018-04-16 05:47:58 -0600

    Update schema.rb
    I took out that merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -18,13 +18,9 @@ ActiveRecord::Schema.define(version: 20180414211210) do
   create_table "accessories", force: :cascade do |t|
     t.string "title"
     t.decimal "price"
-<<<<<<< HEAD
     t.text "description"
     t.string "image", default: "https://i1.wp.com/thebeautyinyou.mlethadaniels.net/wp-content/uploads/2017/10/Smart-Indoor-Recumbent-Exercise-Bike-JOROTO-MH20-Cardio-Fitness-Cycling-Machine-Home-Stationary-Trainer-with-Pulse-0.jpg?resize=150%2C150"
     t.string "status"
-=======
-    t.string "image"
->>>>>>> added stations dashboard namespace, controller, view and tests
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
