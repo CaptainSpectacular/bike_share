a4cfe8a073fe7e061211fb52946aacf91aaa1931
pollockcl <pollock.clee@gmail.com>
2018-04-16 19:52:11 -0600

    added presenter for trips dashboard

--- app/controllers/trips/dashboard_controller.rb
+++ app/controllers/trips/dashboard_controller.rb
@@ -1,20 +1,7 @@
 module Trips
   class DashboardController < ApplicationController
     def index
-      @best_weather  = Trip.best_weather
-      @worst_weather = Trip.worst_weather
-      @avg_duration = Trip.average_duration
-      @min_duration = Trip.min_duration
-      @max_duration = Trip.max_duration
-      @trips_by_month = Trip.beautify_group_by_month
-      @popular_starting_station = Trip.popular_starting_station
-      @popular_ending_station = Trip.popular_ending_station
-      @popular_bike = Trip.popular_bike
-      @unpopular_bike = Trip.unpopular_bike
-      @customer_breakdown = Trip.subscribtion_breakdown('Customer')
-      @subscriber_breakdown = Trip.subscribtion_breakdown('Subscriber')
-      @busy_date = Trip.busy_date
-      @dead_date = Trip.dead_date
+      @presenter = TripsDashboardPresenter.new
     end
   end
 end
