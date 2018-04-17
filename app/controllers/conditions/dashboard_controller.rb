0633c8a93b086f08c42cc4d1763caeb9538896b0
pollockcl <pollock.clee@gmail.com>
2018-04-16 20:01:42 -0600

    conditions dashboard presenter

--- app/controllers/conditions/dashboard_controller.rb
+++ app/controllers/conditions/dashboard_controller.rb
@@ -1,10 +1,7 @@
 module Conditions
   class DashboardController < ApplicationController
     def index
-      @trip_temp_breakdown    = Condition.trips_temp_breakdown
-      @trip_pre_breakdown     = Condition.trips_precipitation_breakdown
-      @trip_wind_breakdown    = Condition.trips_windspeed_breakdown
-      @trip_visible_breakdown = Condition.trips_visibility_breakdown
+      @presenter = ConditionsDashboardPresenter.new
     end
   end
 end
